80bc02c2edda40990d973c211de4c5958f4a7752
Gagan Kaul <gagankaul@gmail.com>
2022-06-17 15:03:44 -0400

    refactor

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,75 +1,69 @@
 #Welcome to the Tic Tac Toe Game
 
-def draw_board(current_list):
-  """Draw the columns in a row"""  
-  print("\n\t\t   Tic-Tac-Toe")
-  print("\t\t------------------")
-  print(f"\t\t|| {current_list[0]} || {current_list[1]} || {current_list[2]} ||")
-  print("\t\t------------------")
-  print(f"\t\t|| {current_list[3]} || {current_list[4]} || {current_list[5]} ||")
-  print("\t\t------------------")
-  print(f"\t\t|| {current_list[6]} || {current_list[7]} || {current_list[8]} ||")
-  print("\t\t------------------")
+
+def draw_board(input_list):
+  """Create a 3x3 grid; either a number board or a tic tac toe game game board"""  
+  print("\n\t   Tic-Tac-Toe")
+  print("\t------------------")
+  print(f"\t|| {input_list[0]} || {input_list[1]} || {input_list[2]} ||")
+  print("\t------------------")
+  print(f"\t|| {input_list[3]} || {input_list[4]} || {input_list[5]} ||")
+  print("\t------------------")
+  print(f"\t|| {input_list[6]} || {input_list[7]} || {input_list[8]} ||")
+  print("\t------------------")
 
 
 def get_player_input(player_char, pos_list):
-  """Get player's move and check if it is valid"""
+  """Get player's move until it is valid on the board with no piece currently there."""
   running = True
   while running:
-    position = int(input(f"\n{player_char}: Where would you like to place the piece (1-9): ")) - 1
-    if pos_list[position] != '_':
-      print("This spot is already taken. Please try again")
+    player_move = int(input(f"\n{player_char}: Where would you like to place the piece (1-9): "))
+    if player_move > 0 and player_move < 10:
+      position = player_move - 1
+      if pos_list[position] != '_':
+        print("This spot is already taken. Please try again")
+      else:
+        pos_list[position] = player_char
+        running = False
     else:
-      pos_list[position] = player_char
-      running = False
+      print("That is not a spot on the board. Please try again.")
 
 
-def is_winner(pos_list, player_char):
-  """Check if a player has won by checking game positions"""
-  if pos_list[0] == player_char and pos_list[1] == player_char and pos_list[2] == player_char:
-    return True
-  elif pos_list[3] == player_char and pos_list[4] == player_char and pos_list[5] == player_char:
-    return True
-  elif pos_list[6] == player_char and pos_list[7] == player_char and pos_list[8] == player_char:
-    return True
-  elif pos_list[0] == player_char and pos_list[3] == player_char and pos_list[6] == player_char:
-    return True
-  elif pos_list[1] == player_char and pos_list[4] == player_char and pos_list[7] == player_char:
-    return True
-  elif pos_list[2] == player_char and pos_list[5] == player_char and pos_list[8] == player_char:
-    return True
-  elif pos_list[0] == player_char and pos_list[4] == player_char and pos_list[8] == player_char:
-    return True
-  elif pos_list[2] == player_char and pos_list[4] == player_char and pos_list[6] == player_char:
-    return True
-  else:
-    return False
+def is_winner(pC, cL):
+  """Return a Bool if the given player is a winner"""
+  return ((cL[0] == pC and cL[1] == pC and cL[2] == pC) or #victory in first row
+          (cL[3] == pC and cL[4] == pC and cL[5] == pC) or #victory in second row
+          (cL[6] == pC and cL[7] == pC and cL[8] == pC) or #victory in third row
+          (cL[0] == pC and cL[3] == pC and cL[6] == pC) or #victory in first column
+          (cL[1] == pC and cL[4] == pC and cL[7] == pC) or #victory in second column
+          (cL[2] == pC and cL[5] == pC and cL[8] == pC) or #victory in third column
+          (cL[0] == pC and cL[4] == pC and cL[8] == pC) or #victory in digonal 1
+          (cL[2] == pC and cL[4] == pC and cL[6] == pC))   #victory in diagonal 2
 
-#Create a list of players
+#The main program loop
+
+#Define variables
 players = ["X", "O"]
+num_list = ["1","2","3","4","5","6","7","8","9"]
+char_list = ["_"]*9
 
-#Create a list of positions to be displayed as the positions grid
-n_list = ["1","2","3","4","5","6","7","8","9"]
+#Draw the initial state of the game board
+draw_board(num_list)
+draw_board(char_list)
 
-#Create an empty list of positions that will be displayed as current game grid
-c_list = ["_"]*9
-
-#Display positions grid
-draw_board(n_list)
-
-#Display game grid
-draw_board(c_list)
-
-#Main program loop to play the game
-no_winner = True
-while no_winner:
+#Program loop to play the game
+state = True
+while state:
   for player in players:
-    get_player_input(player, c_list)
-    draw_board(n_list)
-    draw_board(c_list)
-    if is_winner(c_list, player):
-      print(f"Congratulations! {player} has won this game.")
-      no_winner = False
+    get_player_input(player, char_list)
+    #Redraw game boards
+    draw_board(num_list)
+    draw_board(char_list)
+    if is_winner(player, char_list):
+      print(f"\nCongratulations! {player} has won this game.")
+      state = False
       break
-    elif "_" not in c_list:
-      print("It's a tie")
+    elif "_" not in char_list:
+      print("\nThis game is a tie")
+      state = False
+      break
